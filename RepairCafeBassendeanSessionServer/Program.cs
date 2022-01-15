18d9767c5a4c30b58060d6520a8f9e1b5f1a9251
Mitchell Hawks <mitchhawks@hotmail.com>
2022-01-15 11:21:18 +0800

    Update Proto and versioning

--- RepairCafeBassendeanSessionServer/Program.cs
+++ RepairCafeBassendeanSessionServer/Program.cs
@@ -1,4 +1,7 @@
 using RepairCafeBassendeanSessionServer.Services;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,35 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<PrintItemService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+Console.WriteLine(@" ____  _____ ____   _    ___ ____     ____    _    _____ _____ 
+|  _ \| ____|  _ \ / \  |_ _|  _ \   / ___|  / \  |  ___| ____|
+| |_) |  _| | |_) / _ \  | || |_) | | |     / _ \ | |_  |  _|  
+|  _ <| |___|  __/ ___ \ | ||  _ <  | |___ / ___ \|  _| | |___ 
+|_| \_\_____|_| /_/   \_\___|_| \_\  \____/_/   \_\_|   |_____|");
+Console.WriteLine(@" _                           
+|_) _. _ _ _ ._  _| _  _.._  
+|_)(_|_>_>(/_| |(_|(/_(_|| | 
+                             ");
+
+Console.WriteLine("Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
+Console.WriteLine();
+
+Console.WriteLine("Starting server...");
+var host = Dns.GetHostEntry(Dns.GetHostName());
+Console.WriteLine("Server IP address is");
+foreach (var ip in host.AddressList)
+{
+    if (ip.AddressFamily == AddressFamily.InterNetwork)
+    {
+        Console.WriteLine(ip.ToString());
+        app.Urls.Add("http://" + ip.ToString() + ":5000");
+    }
+}
+Console.WriteLine("Enter the IP address above on each computer.");
+
+Console.WriteLine();
 
 app.Run();
---
+++ RepairCafeBassendeanSessionServer/Services/PrintItemService.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Core;
+using RepairCafeBassendeanSessionServer;
+using bpac;
+
+namespace RepairCafeBassendeanSessionServer.Services
+{
+    public class PrintItemService : PrintItem.PrintItemBase
+    {
+        private readonly ILogger<PrintItemService> _logger;
+        private const string TEMPLATE_DIRECTORY = @"C:\RCBAPP\Templates\"; // Template file path
+        private const string TEMPLATE_SIMPLE = "testrcb.lbx";    // Template file name
+
+        public PrintItemService(ILogger<PrintItemService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<PrintReply> PrintItem(RepairItemRequest request, ServerCallContext context)
+        {
+            string templatePath = TEMPLATE_DIRECTORY;
+            templatePath += TEMPLATE_SIMPLE;// None decoration frame
+            string result;
+
+            bpac.Document doc = new();
+            if (doc.Open(templatePath) != false)
+            {
+                doc.GetObject("objCustName").Text = request.Owner;
+                doc.GetObject("objItem").Text = request.Item;
+                doc.GetObject("objItemNo").Text = request.Id;
+
+                // doc.SetMediaById(doc.Printer.GetMediaId(), true);
+                doc.StartPrint("", PrintOptionConstants.bpoDefault);
+                doc.PrintOut(1, PrintOptionConstants.bpoDefault);
+                doc.EndPrint();
+                doc.Close();
+                _logger.LogInformation("Successfully printed item");
+                result = "Success!";
+            }
+            else
+            {
+                _logger.LogWarning("Error printing: " + doc.ErrorCode);
+                result = "Error printing: " + doc.ErrorCode;
+            }
+
+            return Task.FromResult(new PrintReply
+            {
+                Messages = result
+            });
+        }
+    }
+}
